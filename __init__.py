1a42c03cb8364a3aca86615bf2f083519718c104
SerkanHub <ozerserkan05@gmail.com>
2026-01-05 15:09:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,11 +17,23 @@ def MaPremiereAPI():
 
 @app.route('/tawarano/')
 def meteo():
-    return render_template("tawarano.html")
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def tawaranoTemp():
+    return render_template("tawarano.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
